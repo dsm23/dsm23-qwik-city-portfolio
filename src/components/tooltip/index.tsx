845cbf188279470bb23a36b830a2954fc149825a
David Murdoch <dsmurdoch357@gmail.com>
2023-12-20 15:00:41 +0000

    Add tooltip

---
+++ src/components/tooltip/index.tsx
@@ -0,0 +1,22 @@
+import { Slot, component$ } from "@builder.io/qwik";
+import type { ClassList, HTMLAttributes } from "@builder.io/qwik";
+
+import styles from "./styles.module.css";
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  class: ClassList;
+}
+
+const Tooltip = component$<Props>((props) => (
+  <div
+    data-tip-position="top"
+    {...props}
+    inert={true}
+    role="tooltip"
+    class={[styles.tooltip, props.class]}
+  >
+    <Slot />
+  </div>
+));
+
+export default Tooltip;
