87b89c74120c03db823f5f2e5065d288028f6052
David Murdoch <dsmurdoch357@gmail.com>
2023-12-22 22:52:01 +0000

    Add build stage to pipeline

--- src/hooks/useClickOutside/index.ts
+++ src/hooks/useClickOutside/index.ts
@@ -1,11 +1,12 @@
 import { $, useOnDocument } from "@builder.io/qwik";
-import type { Signal } from "@builder.io/qwik";
+import type { QRL, Signal } from "@builder.io/qwik";
 
-const useClickOutside = (el: Signal<HTMLElement>, handler: Function) => {
-  const handleClick = (e: MouseEvent) =>
-    !el.value.contains(e.target as Node) && handler?.();
+const useClickOutside = (el: Signal<HTMLElement>, handler: QRL<Function>) => {
+  const handleClick = $(
+    (e: MouseEvent) => !el.value.contains(e.target as Node) && handler?.(),
+  );
 
-  useOnDocument("click", $(handleClick));
+  useOnDocument("click", handleClick);
 };
 
 export default useClickOutside;
