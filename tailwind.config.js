0b8f881385fa743d8530a5dd5470facdb24a1924
David Murdoch <dsmurdoch357@gmail.com>
2023-12-22 22:43:45 +0000

    Add Geist font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
+        sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
