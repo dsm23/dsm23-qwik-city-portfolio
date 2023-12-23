7d69c75ab73a7e92a64eb8efb53fc772607b5e70
David Murdoch <dsmurdoch357@gmail.com>
2023-12-23 14:42:16 +0000

    Add data fetching for skills page

--- src/components/svgs/filled-star/index.tsx
+++ src/components/svgs/filled-star/index.tsx
@@ -1,7 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import type { SVGProps } from "@builder.io/qwik";
+import { twMerge } from "tailwind-merge";
+import type { ClassNameValue } from "tailwind-merge";
 
-type Props = SVGProps<SVGSVGElement>;
+interface Props extends Omit<SVGProps<SVGSVGElement>, "class"> {
+  class?: ClassNameValue;
+}
 
 const FilledStar = component$<Props>((props) => (
   <svg
@@ -9,7 +13,7 @@ const FilledStar = component$<Props>((props) => (
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    class={["text-yellow-500", props.class]}
+    class={twMerge(["h-6 w-6 text-yellow-500", props.class])}
   >
     <path fill="currentColor" d="M12 2l3 7h7l-5 6 2 7-7-4-7 4 2-7-5-6h7l3-7z" />
   </svg>
--- src/routes/skill/[skill]/index.tsx
+++ src/routes/skill/[skill]/index.tsx
@@ -1,9 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
-import { Main, StyledGoBack } from "~/components";
+import { component$, useComputed$ } from "@builder.io/qwik";
+import { routeLoader$, useLocation } from "@builder.io/qwik-city";
+import { EmptyStar, FilledStar, Main, StyledGoBack } from "~/components";
+import { getSkillBySlug } from "~/utils/api";
+
+export const useSkill = routeLoader$(async (requestEvent) => {
+  // This code runs only on the server, after every navigation
+  return await getSkillBySlug(requestEvent.params.skill);
+});
 
 export default component$(() => {
   const loc = useLocation();
+
+  const signal = useSkill();
+
+  const rating = useComputed$<number>(() => signal.value.rating);
+
   return (
     <Main class="w-full px-6 py-8">
       <StyledGoBack class="mb-4" href="/#skills" />
@@ -12,18 +23,25 @@ export default component$(() => {
       <div class="mb-4">
         {/* {documentToSolidComponents(content()?.json, contentfulOptions)} */}
         {loc.params.skill}
+        <pre>{JSON.stringify(signal.value, null, 2)}</pre>
       </div>
 
       <div class="flex">
         <h2 class="tracking-widest text-sky-600">PROFICIENCY:</h2>
 
-        {/* <For each={Array.from({ length: rating() })}>
-          {() => <FilledStar class="h-6 w-6" />}
-        </For>
+        {Array.from(
+          { length: rating.value },
+          (_, i) => `${loc.params.skill}-${i}`,
+        ).map((val) => (
+          <FilledStar key={val} />
+        ))}
 
-        <For each={Array.from({ length: 5 - rating() })}>
-          {() => <EmptyStar class="h-6 w-6" />}
-        </For> */}
+        {Array.from(
+          { length: 5 - rating.value },
+          (_, i) => `${loc.params.skill}-${i}`,
+        ).map((val) => (
+          <EmptyStar key={val} />
+        ))}
       </div>
     </Main>
   );
