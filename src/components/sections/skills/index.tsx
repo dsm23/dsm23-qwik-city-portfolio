fc001fccc1c3523613b29869a53f80119f23f450
David Murdoch <dsmurdoch357@gmail.com>
2024-04-14 16:40:37 +0100

    chore(skills): update skills section styles

--- src/components/sections/skills/index.tsx
+++ src/components/sections/skills/index.tsx
@@ -1,13 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import type { HTMLAttributes } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import type { ClassNameValue } from "tailwind-merge";
 import Section from "~/components/section";
 import Tooltip from "~/components/tooltip";
 import type { Maybe, Skill } from "~/generated/generated";
 
 import styles from "./styles.module.css";
 
-interface Props extends HTMLAttributes<HTMLElement> {
+interface Props extends Omit<HTMLAttributes<HTMLElement>, "class"> {
+  class?: ClassNameValue;
   skills: Maybe<
     Skill & {
       svg: string;
@@ -16,19 +18,15 @@ interface Props extends HTMLAttributes<HTMLElement> {
 }
 
 const Skills = component$<Props>(({ skills = [], ...props }) => (
-  // TODO: change this
-  // @ts-ignore
   <Section {...props}>
     <h2 class="text-5xl">Skills</h2>
-    <div class="flex flex-wrap items-baseline">
+    <div class="flex flex-wrap items-baseline print:gap-2">
       {skills.map((item) => (
         <Link
           href={`/skill/${item?.slug}`}
-          class="group"
+          class="group print:grid print:justify-items-center"
           key={`skills-${item?.skillName}`}
         >
-          <span class="sr-only">{item?.skillName}</span>
-
           <div
             class={[
               styles.iconWrapper,
@@ -36,6 +34,10 @@ const Skills = component$<Props>(({ skills = [], ...props }) => (
             ]}
             dangerouslySetInnerHTML={item?.svg}
           />
+
+          <div class="sr-only print:not-sr-only print:block">
+            {item?.skillName}
+          </div>
 
           <Tooltip>{item?.skillName}</Tooltip>
         </Link>
